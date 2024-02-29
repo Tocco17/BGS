385942309a5c3a548633aaff906bbb34e0f8f7f9
Federico Ruocco <federico.ruocco@remira.com>
2024-02-29 10:45:01 +0100

    test - base select query

---
+++ BoardGameSteps.api/BoardGameSteps.entities/Models/Prova.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGameSteps.entities.Models;
+public class Prova : BaseEntity
+{
+    public string ProvaProperty { get; set; } = null;
+}
---
+++ BoardGameSteps.api/BoardGameSteps.test/Entities/Queries/BaseSelectQueryTest.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+using BoardGameSteps.entities.Contexts;
+using BoardGameSteps.entities.Models;
+using BoardGameSteps.entities.Queries;
+using BoardGameSteps.test.Fixtures;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace BoardGameSteps.test.Entities.Queries;
+public class BaseSelectQueryTest : IClassFixture<DatabaseFixture>
+{
+	private readonly DatabaseFixture _fixture;
+	private readonly BgsDbContext _context;
+
+    public BaseSelectQueryTest(DatabaseFixture fixture)
+    {
+		_fixture = fixture;
+		_context = fixture.DbContext;
+    }
+
+    [Fact]
+	public void TestWrongProperty()
+	{
+		try
+		{
+			var prova = new Prova();
+			var provaType = prova.GetType();
+			var provaProperty = provaType.GetProperty("ProvaProperty");
+
+			if (provaProperty == null)
+				Assert.Fail("Property not found, but it should exists.");
+
+			var userDbSet = _context.Users;
+			if (userDbSet == null)
+				Assert.Fail("User Db is null.");
+
+			var selectQueryObject = new UserSelectQuery
+			{
+				OrderBy = provaProperty
+			};
+			var query = selectQueryObject.GetSelectQuery(userDbSet);
+			Assert.True(false);
+		}
+		catch (ArgumentException)
+		{
+			Assert.True(true);
+		}
+		
+
+	}
+
+	[Fact]
+	public void TestRightProperty()
+	{
+		var user = new User();
+		var userType = user.GetType();
+		var idProperty = userType.GetProperty("Id");
+
+		if (idProperty == null)
+			Assert.Fail("Property not found, but it should exists.");
+
+		var userDbSet = _context.Users;
+		if (userDbSet == null)
+			Assert.Fail("User Db is null.");
+
+		var selectQueryObject = new UserSelectQuery
+		{
+			OrderBy = idProperty
+		};
+		var query = selectQueryObject.GetSelectQuery(userDbSet);
+
+		Assert.True(true);
+	}
+}
