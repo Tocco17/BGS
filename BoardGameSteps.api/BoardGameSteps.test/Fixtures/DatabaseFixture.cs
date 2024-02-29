57f064987269261f98961d61e100960f4e648d2f
Federico Ruocco <federico.ruocco@remira.com>
2024-02-29 10:44:47 +0100

    test database fixture

---
+++ BoardGameSteps.api/BoardGameSteps.test/Fixtures/DatabaseFixture.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BoardGameSteps.entities.Contexts;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BoardGameSteps.test.Fixtures;
+
+public class DatabaseFixture : IDisposable
+{
+	public BgsDbContext DbContext;
+
+	public DatabaseFixture()
+	{
+		var serviceProvider = new ServiceCollection()
+			.AddDbContext<BgsDbContext>(options =>
+				options.UseInMemoryDatabase(databaseName: "TestDatabase"))
+			.BuildServiceProvider();
+
+		DbContext = serviceProvider.GetRequiredService<BgsDbContext>();
+		DbContext.Database.EnsureCreated();
+	}
+
+	public void Dispose()
+	{
+		DbContext.Dispose();
+	}
+}
+
+[CollectionDefinition("Database collection")]
+public class DatabaseCollection : ICollectionFixture<DatabaseFixture>
+{
+	// This class has no code, and is never created. Its purpose is simply
+	// to be the place to apply [CollectionDefinition] and all the
+	// ICollectionFixture<> interfaces.
+}
