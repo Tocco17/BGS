5a0d09965a3e76f6cd9fbb5183dd153ddbee8385
Federico Ruocco <federico.ruocco@remira.com>
2024-03-05 15:15:15 +0100

    base insert query test

--- BoardGameSteps.api/BoardGameSteps.test/Entities/Tests/Queries/BaseInsertQueryTest.cs
+++ BoardGameSteps.api/BoardGameSteps.test/Entities/Tests/Queries/BaseInsertQueryTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,26 +9,30 @@ using BoardGameSteps.entities.Exceptions;
 using BoardGameSteps.test.Entities.Base.Contextes;
 using BoardGameSteps.test.Entities.Base.Entities;
 using BoardGameSteps.test.Entities.Base.Queries;
+using BoardGameSteps.test.Entities.Base.Repositories;
 using BoardGameSteps.test.Fixtures;
 
 namespace BoardGameSteps.test.Entities.Tests.Queries;
 public class BaseInsertQueryTest : IClassFixture<DatabaseFixture>
 {
 	private readonly GenericDbContext _context;
+	private readonly GenericRepository _repo;
 
 	public BaseInsertQueryTest(DatabaseFixture fixture)
 	{
 		_context = fixture.DbContext;
+		_repo = new GenericRepository(fixture.DbContext);
 	}
 
 	[Fact]
-	public void TestValidate()
+	public async Task TestValidate()
 	{
 		try
 		{
 			var entity = new GenericEntity();
 			var insertQuery = new GenericInsertQuery(entity);
-			insertQuery.Validate();
+			await _repo.InsertAsync(insertQuery);
+			Assert.Fail("It should have gone in error.");
 		}
 		catch (InsertQueryValidateException)
 		{
@@ -40,8 +45,91 @@ public class BaseInsertQueryTest : IClassFixture<DatabaseFixture>
 	}
 
 	[Fact]
-	public void TestInsert()
+	public async Task TestInsert()
 	{
+		var entity = new GenericEntity
+		{
+			StringGenericEntityProperty = "test",
+		};
+		var insertQuery = new GenericInsertQuery(entity);
+		var count = await _repo.InsertAsync(insertQuery);
 
+		if(count != 1)
+			Assert.Fail($"Count of inserted entities is {count}, while it should be 1.");
+
+		Assert.True(true);
+	}
+
+	[Fact]
+	public async Task TestNewGuid()
+	{
+		var entity = new GenericEntity
+		{
+			StringGenericEntityProperty = "test",
+		};
+		var insertQuery = new GenericInsertQuery(entity);
+
+		var initialGuid = entity.Id;
+
+		await _repo.InsertAsync(insertQuery);
+
+		var newGuid = entity.Id;
+
+		if(newGuid == Guid.Empty)
+			Assert.Fail("The guid is empty.");
+
+		Assert.True(true);
+	}
+
+	[Fact]
+	public async Task TestDuplicateGuid()
+	{
+		var firstEntity = new GenericEntity
+		{
+			StringGenericEntityProperty = "test",
+		};
+		var firstInsertQuery = new GenericInsertQuery(firstEntity);
+
+		await _repo.InsertAsync(firstInsertQuery);
+
+		var firstId = firstEntity.Id;
+
+		var secondEntity = new GenericEntity
+		{
+			Id = firstId,
+			StringGenericEntityProperty = "test",
+		};
+		var secondInsertQuery = new GenericInsertQuery(secondEntity);
+
+		await _repo.InsertAsync(secondInsertQuery);
+
+		if (firstEntity.Id == secondEntity.Id)
+			Assert.Fail("Id can't be duplicate");
+
+		Assert.True(true);
+	}
+
+	[Fact]
+	public async Task TestSelectAfterInsert()
+	{
+		var entity = new GenericEntity
+		{
+			StringGenericEntityProperty = "test",
+		};
+		var insertQuery = new GenericInsertQuery(entity);
+		await _repo.InsertAsync(insertQuery);
+
+		var id = entity.Id;
+
+		var query = new GenericSelectQuery()
+		{
+			Id = id,
+		};
+		var selectedEntity = await _repo.SelectAsync(query);
+
+		if (selectedEntity.Count() != 1)
+			Assert.Fail("No entity has been found.");
+
+		Assert.True(true);
 	}
 }
