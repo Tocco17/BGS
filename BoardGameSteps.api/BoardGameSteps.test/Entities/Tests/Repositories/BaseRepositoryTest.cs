a4f5df731662f4f57b626a24074960edd701213f
Federico Ruocco <federico.ruocco@remira.com>
2024-03-05 14:46:27 +0100

    base repository test

---
+++ BoardGameSteps.api/BoardGameSteps.test/Entities/Tests/Repositories/BaseRepositoryTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BoardGameSteps.test.Entities.Base.Contextes;
+using BoardGameSteps.test.Entities.Base.Repositories;
+using BoardGameSteps.test.Fixtures;
+
+namespace BoardGameSteps.test.Entities.Tests.Repositories;
+public class BaseRepositoryTest : IClassFixture<DatabaseFixture>
+{
+	private readonly GenericDbContext _context;
+	private readonly GenericRepository _repo;
+
+	public BaseRepositoryTest(DatabaseFixture fixture)
+	{
+		_context = fixture.DbContext;
+		_repo = new GenericRepository(fixture.DbContext);
+	}
+
+	[Fact]
+	public async Task TestSelectFixture()
+	{
+		var entities = await _repo.SelectAsync();
+
+		if (entities == null)
+			Assert.Fail("Entities are null.");
+
+		Assert.True(true);
+	}
+
+}
